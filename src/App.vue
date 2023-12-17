6ef7076b69b36bdbebf1ab873224b567146047f5
romanofficial <roman.shaon@gmail.com>
2023-12-18 03:36:07 +0600

    Non Prop Attributes

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,25 @@
 <template>
-    <HelloWorld name="roman" heroName="superMan"/>
-    <HelloWorld name="hossain" heroName="batMan"/>
-    <HelloWorld :name="name" :heroName="channel"/>
+  <HelloWorld name="roman" heroName="superMan" />
+  <HelloWorld name="hossain" heroName="batMan" />
+  <HelloWorld :name="name" :heroName="channel" />
 
-    <Artical title="Artical title" :likes="500" description="lorem imsum" />
-
+  <Artical id="my-artical" title="Artical title" :likes="500" description="lorem imsum" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-import Artical from './components/Artical.vue';
+import HelloWorld from "./components/HelloWorld.vue";
+import Artical from "./components/Artical.vue";
 
-export default{
-  components:{
+export default {
+  components: {
     HelloWorld,
-    Artical
+    Artical,
   },
-data() {
-  return {
-    name:"github",
-    channel: "opensource"
-  }
-},
-}
+  data() {
+    return {
+      name: "github",
+      channel: "opensource",
+    };
+  },
+};
 </script>
--- src/components/Artical.vue
+++ src/components/Artical.vue
@@ -1,19 +1,23 @@
 <template>
   <div>
-    <h2>{{ title }} {{likes}} {{description}}</h2>
+    <h2>{{ title }} {{ likes }} {{ description }}</h2>
+    <h2 v-bind="$attrs">{{ title }} {{ likes }} {{ description }}</h2>
+    <h2>{{ title }} {{ likes }} {{ description }}</h2>
+    <h2>{{ title }} {{ likes }} {{ description }}</h2>
   </div>
 </template>
 
 <script>
-export default{
-  props:{
-    title:String,
-    likes:Number,
-    description:{
+export default {
+  props: {
+    title: String,
+    likes: Number,
+    description: {
       type: String,
       required: true,
       default: "props missing",
-    }
-}
-}
+    },
+  },
+  inheritAttrs:false,
+};
 </script>
