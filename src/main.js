c42f465bc90082db2f8d153ac4edf5f768606bdc
romanofficial <roman.shaon@gmail.com>
2023-12-18 02:08:47 +0600

    defineProps App.vue, HelloWorld.vue,

--- src/main.js
+++ src/main.js
@@ -1,10 +1,3 @@
-// import './assets/main.css'
-import './assets/css/bootstrap.min.css'
-import './assets/css/templatemo-tiya-golf-club.css'
-import './assets/css/bootstrap-icons.css'
-
-
-
 import { createApp } from 'vue'
 import App from './App.vue'
 
