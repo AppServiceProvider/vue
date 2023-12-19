f55c3026e0c2e1f9e02cc8e87f520f88e9cdb12d
romanofficial <roman.shaon@gmail.com>
2023-12-19 22:19:08 +0600

    Add input field to ChildTwo component

--- src/components/ChildTwo.vue
+++ src/components/ChildTwo.vue
@@ -1,10 +1,18 @@
 <template>
     <div>
       <h1>Hello user2</h1>
+      <input type="text" v-model="name">
     </div>
   </template>
   
-  <script setup>
-  
+  <script>
+    export default{
+      name: 'ChildTwo',
+      data(){
+        return {
+          name: ''
+      }
+    }
+  }
   </script>
   
