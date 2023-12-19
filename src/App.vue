f2e5b04f542d14510aa11e37cb8be224b72a8eba
romanofficial <roman.shaon@gmail.com>
2023-12-19 21:52:35 +0600

    Dynamic Components

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,33 @@
 <template>
-  <ChildComponent v-slot="slotProps">Name: {{ slotProps.firstName }} {{ slotProps.lastName }}</ChildComponent>
+  <button @click="activeTab = 'TabA'">tab A</button>
+  <button @click="activeTab = 'TabB'">tab B</button>
+  <button @click="activeTab = 'TabC'">tab C</button>
+
+  <ChaildOne v-if="activeTab === 'TabA'" />
+  <ChaildTwo v-if="activeTab === 'TabB'" />
+  <ChaildThree v-if="activeTab === 'TabC'" />
 </template>
-<script>
-import ChildComponent from "./components/ChaildComponent.vue";
+
+<script >
+import ChaildOne from "./components/ChaildOne.vue";
+import ChaildTwo from "./components/ChaildTwo.vue";
+import ChaildThree from "./components/ChaildThree.vue";
+
+
 export default {
   name: "App",
-  components: {
-    ChildComponent,
-  },
-};
+  components:{
+        ChaildOne,
+        ChaildTwo,
+        ChaildThree,
+
+    },
+    data(){
+        return{
+          activeTab: 'TabA',
+      }
+  }
+}
 </script>
+
 <style></style>
---
+++ src/components/ChaildOne.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>Hello user1</h1>
+  </div>
+</template>
+
+<script setup>
+
+</script>
