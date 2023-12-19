a1dca7beb101293df83f5caf14f9471f181d0c4e
romanofficial <roman.shaon@gmail.com>
2023-12-19 22:06:02 +0600

    Renamed TabA, TabB, TabC to ChildOne, ChildTwo, ChildThree

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,33 @@
 <template>
-  <button @click="activeTab = 'TabA'">tab A</button>
-  <button @click="activeTab = 'TabB'">tab B</button>
-  <button @click="activeTab = 'TabC'">tab C</button>
+  <button @click="activeTab = 'ChildOne'">tab A</button>
+  <button @click="activeTab = 'ChildTwo'">tab B</button>
+  <button @click="activeTab = 'ChildThree'">tab C</button>
 
-  <ChaildOne v-if="activeTab === 'TabA'" />
-  <ChaildTwo v-if="activeTab === 'TabB'" />
-  <ChaildThree v-if="activeTab === 'TabC'" />
+
+
+  <ChildOne v-if="activeTab === 'ChildOne'" />
+  <ChildTwo v-if="activeTab === 'ChildTwo'" />
+  <ChildThree v-if="activeTab === 'ChildThree'" />
+
 </template>
 
 <script >
-import ChaildOne from "./components/ChaildOne.vue";
-import ChaildTwo from "./components/ChaildTwo.vue";
-import ChaildThree from "./components/ChaildThree.vue";
+import ChildOne from "./components/ChildOne.vue";
+import ChildTwo from "./components/ChildTwo.vue";
+import ChildThree from "./components/ChildThree.vue";
 
 
 export default {
   name: "App",
   components:{
-        ChaildOne,
-        ChaildTwo,
-        ChaildThree,
+        ChildOne,
+        ChildTwo,
+        ChildThree,
 
     },
     data(){
         return{
-          activeTab: 'TabA',
+          activeTab: 'ChildOne',
       }
   }
 }
